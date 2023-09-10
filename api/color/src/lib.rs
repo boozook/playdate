a68e4e4ad2a6f0ae718f85c9367b36d2207f18da
Alexander Koz <a@koz.email>
2023-09-10 20:26:27 +0400

    add license to crate-manifests

--- api/color/src/lib.rs
+++ api/color/src/lib.rs
@@ -2,7 +2,7 @@
 #![feature(const_trait_impl)]
 #![feature(impl_trait_in_assoc_type)]
 
-pub extern crate sys;
+extern crate sys;
 use sys::ffi::LCDColor;
 use sys::ffi::LCDPattern;
 use sys::ffi::LCDSolidColor;
