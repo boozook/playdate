20716f1c79d3ccc45e414fe4575de5c788b1fb4e
Alexander Koz <a@koz.email>
2023-09-13 18:15:02 +0400

    removed extra `ll_symbols` for bin

--- cargo/tests/crates/simple/bin.rs
+++ cargo/tests/crates/simple/bin.rs
@@ -6,6 +6,5 @@ extern crate game;
 use game::eventHandlerShim;
 
 #[used]
-pub static EVENT_HANDLER_SHIM: extern "C" fn(*const pd::ffi::PlaydateAPI, pd::ffi::PDSystemEvent, u32) -> i32 = eventHandlerShim;
-
-pd::ll_symbols!();
+pub static EVENT_HANDLER_SHIM: extern "C" fn(*const pd::ffi::PlaydateAPI, pd::ffi::PDSystemEvent, u32) -> i32 =
+	eventHandlerShim;
