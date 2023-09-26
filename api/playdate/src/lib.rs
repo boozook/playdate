a40ec16a95354f2a44483468490168e6a41317a9
Alexander Koz <a@koz.email>
2023-09-26 16:53:59 +0400

    add cached api end-point for display

--- api/playdate/src/lib.rs
+++ api/playdate/src/lib.rs
@@ -33,3 +33,34 @@ pub mod fs {
 	pub use fs::*;
 	pub use fs::prelude::*;
 }
+
+
+pub mod ext {
+	use core::ptr::NonNull;
+
+
+	pub trait PlaydateAPIExt {
+		// fn system() -> system::System;
+		// fn file() -> file::File;
+		// fn graphics() -> graphics::Graphics;
+		// fn sprite() -> sprite::Sprite;
+		fn display(&self) -> display::Display<display::api::Cache>;
+		// fn sound() -> sound::Sound;
+		// fn lua() -> lua::Lua;
+		// fn json() -> json::Json;
+		// fn scoreboards() -> scoreboards::Scoreboards;
+	}
+
+
+	impl PlaydateAPIExt for NonNull<sys::ffi::PlaydateAPI> {
+		fn display(&self) -> display::Display<display::api::Cache> {
+			display::Display::new_with(display::api::Cache::from(unsafe { self.as_ref() }.display))
+		}
+	}
+
+	impl PlaydateAPIExt for *const sys::ffi::PlaydateAPI {
+		fn display(&self) -> display::Display<display::api::Cache> {
+			display::Display::new_with(display::api::Cache::from(unsafe { self.as_ref() }.expect("api").display))
+		}
+	}
+}
