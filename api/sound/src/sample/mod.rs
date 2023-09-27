9df3139ba1186a3ad6129220a51d27dbb2840060
Alexander Koz <a@koz.email>
2023-09-27 20:41:08 +0400

    missed code format

--- api/sound/src/sample/mod.rs
+++ api/sound/src/sample/mod.rs
@@ -38,9 +38,7 @@ impl Sample {
 	///
 	/// __Does not loads a file.__
 	pub fn new_for_file<P: AsRef<Path>>(path: P) -> Self {
-		let size = {
-			fs::metadata(path).expect("fs metadata").size
-		};
+		let size = { fs::metadata(path).expect("fs metadata").size };
 
 		Self::new_with_size(size as _)
 	}
