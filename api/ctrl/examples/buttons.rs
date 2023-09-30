b1d7962a8351b0a88d9a8ddf62069c424293dbc1
Alexander Koz <a@koz.email>
2023-09-30 16:07:00 +0400

    Improve controls examples

--- api/ctrl/examples/buttons.rs
+++ api/ctrl/examples/buttons.rs
@@ -1,23 +1,29 @@
 #![no_std]
-#[macro_use]
 extern crate alloc;
-use core::ffi::*;
+use alloc::format;
+use alloc::vec::Vec;
 use alloc::borrow::Cow;
-use alloc::boxed::Box;
+use core::ptr::NonNull;
 
 #[macro_use]
 extern crate sys;
 extern crate playdate_controls as controls;
-use alloc::format;
-use alloc::vec::Vec;
+
 use controls::buttons::PDButtonsExt;
 use controls::buttons::PDButtonsIter;
-use crate::controls::buttons::IterSingleButtons;
-use sys::ffi::*;
+use controls::buttons::IterSingleButtons;
+use controls::peripherals::Buttons;
+
+use display::Display;
+use gfx::color::Color;
+use sys::EventLoopCtrl;
+use sys::ffi::PlaydateAPI;
+use sys::error::NullPtrError;
+use system::prelude::*;
 
 
-const INITIAL_X: u32 = LCD_COLUMNS / 2;
-const INITIAL_Y: u32 = (sys::ffi::LCD_ROWS - TEXT_HEIGHT) / 2;
+const INITIAL_X: u32 = Display::COLUMNS / 2;
+const INITIAL_Y: u32 = (Display::ROWS - TEXT_HEIGHT) / 2;
 const TEXT_HEIGHT: u32 = 16;
 
 
@@ -32,133 +38,80 @@ impl<T> Point<T> {
 }
 
 
-/// App state
-struct State {
-	pos: Point<i32>,
-}
-
-impl State {
-	const fn new() -> Self { Self { pos: Point::new(INITIAL_X as _, INITIAL_Y as _) } }
-
-
-	/// Updates the state
-	fn update(&mut self) -> Option<()> {
-		// get buttons state
-		let buttons = controls::peripherals::Buttons::get()?;
-
-		unsafe {
-			let graphics = (*sys::API).graphics;
-			(*graphics).clear?(LCDSolidColor::kColorWhite as LCDColor);
-
-			// render buttons state to string
-			let text: Cow<str> = if buttons.current.is_empty() {
-				"[Press any button]".into()
-			} else {
-				format!("{:?}", buttons.current.iter().singles().collect::<Vec<_>>()).into()
-			};
-
-			let c_text = CString::new(text.as_ref()).ok()?;
-			// get width (screen-size) of text
-			let text_width = (*graphics).getTextWidth?(
-			                                           core::ptr::null_mut(),
-			                                           c_text.as_ptr() as *const _,
-			                                           text.len(),
-			                                           PDStringEncoding::kUTF8Encoding,
-			                                           0,
-			);
-			// render text
-			(*graphics).drawText?(
-			                      c_text.as_ptr() as *const _,
-			                      text.len(),
-			                      PDStringEncoding::kUTF8Encoding,
-			                      self.pos.x,
-			                      self.pos.y,
-			);
-
-			// move the label
-			const SPEED: i32 = 4;
-			if buttons.current.right() {
-				self.pos.x += SPEED;
-			}
-			if buttons.current.left() {
-				self.pos.x -= SPEED;
-			}
-			if buttons.current.up() {
-				self.pos.y -= SPEED;
-			}
-			if buttons.current.down() {
-				self.pos.y += SPEED;
-			}
-
-			// check screen boundaries
-			if self.pos.x < 0 {
-				self.pos.x = 0
-			} else if self.pos.x > LCD_COLUMNS as i32 - text_width {
-				self.pos.x = LCD_COLUMNS as i32 - text_width
-			}
-			if self.pos.y < 0 {
-				self.pos.y = 0
-			} else if self.pos.y > LCD_ROWS as i32 - TEXT_HEIGHT as i32 {
-				self.pos.y = LCD_ROWS as i32 - TEXT_HEIGHT as i32
-			}
-
-			(*(*sys::API).system).drawFPS?(0, 0);
-			Some(())
-		}
-	}
-
-
-	/// Event handler
-	fn event(&mut self, event: PDSystemEvent) -> Option<()> {
-		match event {
-			// initial setup
-			PDSystemEvent::kEventInit => unsafe {
-				(*(*sys::API).display).setRefreshRate?(20.0);
-			},
-			_ => {},
-		}
-		Some(())
-	}
-}
-
-
+/// Entry point / event handler
 #[no_mangle]
-/// Proxy event handler, calls `State::event`
-pub extern "C" fn eventHandlerShim(api: *const PlaydateAPI, event: PDSystemEvent, _arg: u32) -> c_int {
-	static mut STATE: Option<Box<State>> = None;
-
-	match event {
-		PDSystemEvent::kEventInit => unsafe {
-			// register the API entry point
-			sys::API = api;
-
-			// create game state
-			if STATE.is_none() {
-				STATE = Some(Box::new(State::new()));
-			}
-			let state = STATE.as_mut().unwrap().as_mut() as *mut State;
-
-			// get `setUpdateCallback` fn
-			let f = (*(*api).system).setUpdateCallback.expect("setUpdateCallback");
-			// register update callback with user-data = our state
-			f(Some(on_update), state.cast());
-		},
-		_ => {},
+fn event_handler(_: NonNull<PlaydateAPI>, event: SystemEvent, _: u32) -> EventLoopCtrl {
+	// Ignore any other events, just for this minimalistic example
+	if !matches!(event, SystemEvent::Init) {
+		return EventLoopCtrl::Continue;
 	}
 
-	if let Some(state) = unsafe { STATE.as_mut() } {
-		state.event(event).and(Some(0)).unwrap_or(1)
-	} else {
-		1
-	}
-}
+	Display::Default().set_refresh_rate(20.);
 
+	// State of the example app:
+	let pos = Point::new(INITIAL_X as i32, INITIAL_Y as i32);
 
-/// Proxy update callback, calls `State::update`
-unsafe extern "C" fn on_update(state: *mut c_void) -> i32 {
-	let ptr: *mut State = state.cast();
-	let state = ptr.as_mut().expect("missed state");
-	state.update().and(Some(1)).unwrap_or_default()
+	// Create cached end-points that we using every update
+	let system = system::System::Cached();
+	let graphics = gfx::Graphics::Cached();
+
+	// Register update handler
+	// Just to draw current playback position
+	system.set_update_callback_boxed(
+	                                 move |pos| {
+		                                 graphics.clear(Color::WHITE);
+
+		                                 // Get buttons state
+		                                 let buttons = Buttons::get().ok_or(NullPtrError)?;
+
+		                                 // Render buttons state to string
+		                                 let text: Cow<str> = if buttons.current.is_empty() {
+			                                 "[Press any button]".into()
+		                                 } else {
+			                                 format!("{:?}", buttons.current.iter().singles().collect::<Vec<_>>()).into()
+		                                 };
+
+		                                 // Get width (screen-size) of text
+		                                 let font = Default::default();
+		                                 let text_width = graphics.get_text_width(&text, font, 0)?;
+		                                 // Render text
+		                                 graphics.draw_text(text, pos.x, pos.y)?;
+
+		                                 // Move the label position
+		                                 const SPEED: i32 = 4;
+		                                 if buttons.current.right() {
+			                                 pos.x += SPEED;
+		                                 }
+		                                 if buttons.current.left() {
+			                                 pos.x -= SPEED;
+		                                 }
+		                                 if buttons.current.up() {
+			                                 pos.y -= SPEED;
+		                                 }
+		                                 if buttons.current.down() {
+			                                 pos.y += SPEED;
+		                                 }
+
+		                                 // Check screen boundaries
+		                                 if pos.x < 0 {
+			                                 pos.x = 0
+		                                 } else if pos.x > Display::COLUMNS as i32 - text_width {
+			                                 pos.x = Display::COLUMNS as i32 - text_width
+		                                 }
+		                                 if pos.y < 0 {
+			                                 pos.y = 0
+		                                 } else if pos.y > Display::ROWS as i32 - TEXT_HEIGHT as i32 {
+			                                 pos.y = Display::ROWS as i32 - TEXT_HEIGHT as i32
+		                                 }
+
+		                                 system.draw_fps(0, 0);
+
+		                                 UpdateCtrl::Continue
+	                                 },
+	                                 pos,
+	);
+
+	EventLoopCtrl::Continue
 }
 
 
