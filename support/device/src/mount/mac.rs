600af1868f856c0a7ace90458de9c6b2aaa2f800
Alexander Koz <a@koz.email>
2024-04-28 01:42:47 +0400

    Fix deserialisation error when multiple devices and some of them doesn't contain serial-number

--- support/device/src/mount/mac.rs
+++ support/device/src/mount/mac.rs
@@ -182,6 +182,15 @@ fn parse_spusb<F>(
 	                 .into_iter()
 	                 .filter_map(|c| c.items)
 	                 .flatten()
+	                 .filter_map(|item| {
+		                 match item {
+			                 AnyDeviceInfo::Known(info) => Some(info),
+		                    AnyDeviceInfo::Other { name, .. } => {
+			                    trace!("Skip {name}");
+			                    None
+		                    },
+		                 }
+	                 })
 	                 .filter(|item| item.vendor_id == VENDOR_ID_ENC)
 	                 .filter(filter)
 	                 .filter_map(|item| {
@@ -259,7 +268,20 @@ struct SystemProfilerResponse {
 #[derive(Deserialize, Debug)]
 struct ControllerInfo {
 	#[serde(rename = "_items")]
-	items: Option<Vec<DeviceInfo>>,
+	items: Option<Vec<AnyDeviceInfo>>,
+}
+
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+enum AnyDeviceInfo {
+	#[serde(untagged)]
+	Known(DeviceInfo),
+	#[serde(untagged)]
+	Other {
+		#[serde(rename = "_name")]
+		name: String,
+	},
 }
 
 #[derive(Deserialize, Debug)]
@@ -360,6 +382,76 @@ mod tests {
 		};
 
 		assert_eq!(dev.name, "Playdate");
+		assert_eq!(dev.serial, "PDU1-Y000042");
+
+		let vol = dev.volume.now_or_never().unwrap().unwrap();
+		assert_eq!("/Volumes/PLAYDATE", vol.to_string_lossy());
+	}
+
+	/// Tests parsing doc with multiple devices with one "dev of interest"
+	/// that with serial number.
+	#[test]
+	fn parse_spusb_mount_others() {
+		let data = r#"
+		{
+			"SPUSBDataType" : [
+			  {
+				 "_items" : [
+					{
+					  "_name" : "with-sn",
+					  "Media" : [
+						 {
+							"volumes" : [
+							  {
+								 "_name" : "PLAYDATE",
+								 "bsd_name" : "disk9s1",
+								 "mount_point" : "/Volumes/PLAYDATE",
+								 "volume_uuid" : "1AA11111-111A-311A-11A1-1AA111A1A1A1"
+							  }
+							]
+						 }
+					  ],
+					  "serial_num" : "PDU1-Y000042",
+					  "vendor_id" : "0x1331"
+					},
+					{
+					  "_name" : "with-sn-no-media",
+					  "serial_num" : "PDU1-Y000042",
+					  "vendor_id" : "0x1331"
+					},
+					{
+					  "_name" : "no-sn",
+					  "Media" : [
+						 {
+							"volumes" : [
+							  {
+								 "_name" : "PLAYDATE",
+								 "bsd_name" : "disk9s1",
+								 "mount_point" : "/Volumes/PLAYDATE",
+								 "volume_uuid" : "1AA11111-111A-311A-11A1-1AA111A1A1A1"
+							  }
+							]
+						 }
+					  ],
+					  "vendor_id" : "0x1331"
+					},
+					{
+					  "_name" : "no-sn",
+					  "vendor_id" : "0x1331"
+					}
+				 ]
+			  }
+			]
+		 }
+		"#;
+
+		let dev = {
+			let mut devs: Vec<_> = parse_spusb(|_| true, data.as_bytes()).unwrap().collect();
+			assert_eq!(1, devs.len());
+			devs.pop().unwrap()
+		};
+
+		assert_eq!(dev.name, "with-sn");
 		assert_eq!(dev.serial, "PDU1-Y000042");
 
 		let vol = dev.volume.now_or_never().unwrap().unwrap();
