dfd39f34c17a1a413978271da00e52e688dffe4f
Alexander Koz <888526+boozook@users.noreply.github.com>
2024-04-30 22:42:37 +0400

    apply clippy suggestion
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- cargo/src/cli/target.rs
+++ cargo/src/cli/target.rs
@@ -62,7 +62,7 @@ impl Display for PlaydateTarget {
 impl From<&'_ PlaydateTarget> for CompileKind {
 	fn from(value: &'_ PlaydateTarget) -> Self {
 		match value {
-			PlaydateTarget::Device => CompileKind::Target(CompileTarget::new(DEVICE_TARGET).expect(&format!("Target {DEVICE_TARGET} must be valid CompileTarget"))),
+			PlaydateTarget::Device => CompileKind::Target(CompileTarget::new(DEVICE_TARGET).unwrap_or_else(|_| panic!("Target {DEVICE_TARGET} must be valid CompileTarget"))),
 			PlaydateTarget::Simulator => CompileKind::Host,
 		}
 	}
