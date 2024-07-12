549fc34b2dc4ab706c18aae165a9132932750bda
Alexander Koz <a@koz.email>
2024-07-12 21:38:53 +0400

    fix panic, update readme

--- api/sys/src/sys/panic.rs
+++ api/sys/src/sys/panic.rs
@@ -11,23 +11,6 @@ use super::proc::error;
 #[panic_handler]
 fn panic(panic_info: &PanicInfo) -> ! {
 	let mut output = ArrayString::<1024>::new();
-	let payload = if let Some(payload) = panic_info.payload().downcast_ref::<&str>() {
-		payload
-	} else {
-		""
-	};
-
-	let location = panic_info.location();
-	let (module, line) = if let Some(location) = location {
-		(location.file(), location.line())
-	} else {
-		("", 0)
-	};
-
-	let _ = if let Some(message) = panic_info.message() {
-		write!(output, "PANIC: [{module}@{line}] \"{message}\" {payload}\0")
-	} else {
-		write!(output, "PANIC: [{module}@{line}] {payload}\0")
-	};
+	let _ = write!(output, "{panic_info}\0");
 	error(output.as_str());
 }
