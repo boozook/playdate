1885a2e696be6acc375db4e07c3d650786bd25d7
kana <kana-github@qnet.li>
2025-02-16 20:05:17 +0400

    use gen-api-shorthands in playdate-display

--- api/display/src/lib.rs
+++ api/display/src/lib.rs
@@ -5,7 +5,6 @@ use core::ffi::c_float;
 use core::ffi::c_int;
 use core::ffi::c_uint;
 
-
 #[derive(Debug, Clone, Copy)]
 pub struct Display<Api = api::Default>(Api);
 
@@ -48,7 +47,8 @@ impl Display<api::Default> {
 	                                                               bottom: Self::ROWS as _ };
 }
 
-
+pub use shorthands::*;
+#[gen_api_shorthands::gen_shorthands_mod(mod shorthands)]
 impl<Api: api::Api> Display<Api> {
 	/// Returns the width of the display, taking the current scale into account;
 	///
