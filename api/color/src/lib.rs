27d900d64f948546756dfcac7f45cb7d6628f4f9
Alex Koz <a@koz.email>
2025-04-19 02:09:28 +0400

    color: test

--- api/color/src/lib.rs
+++ api/color/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![cfg_attr(not(test), no_main)]
 #![feature(const_trait_impl, const_deref, ptr_as_uninit)]
+#![cfg_attr(test, feature(assert_matches))]
 
 extern crate sys;
 use core::marker::PhantomData;
@@ -10,21 +11,24 @@ use core::ops::DerefMut;
 use core::ptr::NonNull;
 
 use sys::ffi::Color as UnsafeLcdColor;
-use sys::ffi::Pattern;
-use sys::ffi::SolidColor;
+pub use sys::ffi::Pattern;
+pub use sys::ffi::SolidColor;
 
 pub mod fmt;
 pub mod pattern;
 
 
 /// Safe impl of [`LcdColor`](sys::ffi::Color) with preserved lifetime of [`Pattern`].
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 #[repr(transparent)]
 // May be better to use PhantomInvariantLifetime in a future
 pub struct LcdColor<'t>(UnsafeLcdColor, PhantomData<&'t ()>);
 
 impl LcdColor<'_> {
 	const fn new(value: UnsafeLcdColor) -> Self { Self(value, PhantomData) }
+}
+impl From<SolidColor> for LcdColor<'static> {
+	fn from(value: SolidColor) -> Self { Self::new(value as UnsafeLcdColor) }
 }
 impl From<UnsafeLcdColor> for LcdColor<'_> {
 	fn from(value: UnsafeLcdColor) -> Self { Self::new(value) }
@@ -125,4 +129,114 @@ impl const IntoColor<'static> for SolidColor {
 impl<'t> IntoColor<'t> for &'t Pattern {
 	#[inline(always)]
 	fn into_color(self) -> LcdColor<'t> { LcdColor::new(self.as_ptr() as _) }
+}
+
+
+#[cfg(test)]
+#[macro_use]
+extern crate std;
+
+
+#[cfg(test)]
+mod tests {
+	use core::assert_matches::assert_matches;
+
+	use super::*;
+	use crate::pattern::opaque;
+
+	#[test]
+	fn lcd_color_size() {
+		assert_eq!(size_of::<LcdColor<'_>>(), size_of::<UnsafeLcdColor>());
+	}
+
+
+	// Basic solid colors
+	const COLORS: [SolidColor; 4] = [
+	                                 SolidColor::Black,
+	                                 SolidColor::Clear,
+	                                 SolidColor::White,
+	                                 SolidColor::XOR,
+	];
+
+	#[test]
+	fn lcd_color_from_solid() {
+		for c in COLORS {
+			let lcd = LcdColor::from(c);
+			assert_eq!(lcd.0, c as UnsafeLcdColor);
+		}
+	}
+
+	#[test]
+	fn color_from_lcd_color() {
+		for c in COLORS {
+			let color: Color = LcdColor::from(c).into();
+			assert_matches!(color, Color::Solid(color) if color == c);
+		}
+	}
+
+	#[test]
+	fn solid_into_color() {
+		for c in COLORS {
+			let a = LcdColor::from(c);
+			let b = c.into_color();
+			assert_eq!(a, b);
+		}
+	}
+
+
+	pub const PAT: Pattern = opaque([0xf, 0x6f, 0x6f, 0xf, 0xf0, 0xf6, 0xf6, 0xf0]);
+
+	#[test]
+	fn pattern_into_color_static() {
+		let a = PAT.into_color();
+		let provenance_a = PAT.as_ptr().expose_provenance();
+
+		static STATIC: Pattern = PAT;
+		let b = STATIC.into_color();
+
+		let provenance_b = STATIC.as_ptr().expose_provenance();
+
+		{
+			// This is not acceptable for the compiler, so great as should be:
+			// let pat = opaque([0xf, 0x6f, 0x6f, 0xf, 0xf0, 0xf6, 0xf6, 0xf0]);
+			// let c = pat.into_color();
+			// see pattern_into_color_stack test.
+		}
+
+		for _ in 0..100 {
+			for (c, provenance) in [(a, provenance_a), (b, provenance_b)] {
+				accept_any(c, provenance);
+				accept_static_only(c, provenance);
+			}
+		}
+	}
+
+	#[test]
+	fn pattern_into_color_stack() {
+		let pat = opaque([0xf, 0x6f, 0x6f, 0xf, 0xf0, 0xf6, 0xf6, 0xf0]);
+		let provenance = pat.as_ptr().expose_provenance();
+
+		for _ in 0..100 {
+			let c = pat.into_color();
+			accept_any(c, provenance);
+		}
+	}
+
+
+	fn accept_any(c: LcdColor<'_>, provenance: usize) {
+		assert!(c.is_pattern());
+		// simulate pattern internal usage
+		// let ptr = c.0 as *const Pattern;
+
+		// instead, for test we using strict provenance api:
+		let ptr: *const Pattern = core::ptr::with_exposed_provenance::<Pattern>(provenance).with_addr(c.0);
+		assert!(core::ptr::addr_eq(c.0 as *const (), ptr));
+
+		let pat = unsafe { core::ptr::read(ptr) };
+		assert_eq!(PAT, pat);
+	}
+
+	fn accept_static_only(c: LcdColor<'static>, provenance: usize) {
+		std::thread::spawn(move || accept_any(c, provenance)).join().ok();
+	}
 }
