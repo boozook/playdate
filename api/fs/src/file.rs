cbd521d1e19e0835bfc76e0b365b85b85c7f28a7
Alex Koz <a@koz.email>
2025-06-21 13:07:10 +0400

    fs: impl traits of `portable-io` & `no_std_io2` for `File`

--- api/fs/src/file.rs
+++ api/fs/src/file.rs
@@ -1,6 +1,5 @@
 use core::ffi::c_uint;
 use core::ptr::NonNull;
-use alloc::vec::Vec;
 
 use sys::ffi::FileOptions;
 use sys::ffi::SdFile;
@@ -69,7 +68,7 @@ impl File {
 	#[doc(alias = "sys::ffi::PlaydateFile::read")]
 	#[must_use]
 	#[inline(always)]
-	pub fn read(&mut self, to: &mut Vec<u8>, len: c_uint) -> Result<c_uint, Owned> {
+	pub fn read(&mut self, to: &mut [u8], len: c_uint) -> Result<c_uint, Owned> {
 		crate::op::read(api!(file), self, to, len).map_err(Owned::from)
 	}
 
@@ -128,6 +127,129 @@ impl Drop for File {
 			let code = unsafe { (api.close)(self.0) };
 			self.0 = core::ptr::null_mut();
 			err_code_on_drop(code, api);
+		}
+	}
+}
+
+
+#[cfg(any(feature = "no_std_io2", feature = "portable-io"))]
+mod io {
+	use super::File;
+	use alloc::boxed::Box;
+
+	#[cfg(feature = "portable-io")]
+	use portable_io as io;
+	#[cfg(all(feature = "no_std_io2", not(feature = "portable-io")))]
+	use no_std_io2::io;
+
+
+	impl io::Read for File {
+		fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+			debug_assert!(buf.len() <= core::ffi::c_uint::MAX as usize);
+
+			File::read(self, buf, buf.len() as _).map_err(|e| io::Error::other(Box::new(e)))
+			                                     .map(|v| v as usize)
+		}
+
+		fn read_exact(&mut self, mut buf: &mut [u8]) -> io::Result<()> {
+			debug_assert!(buf.len() <= core::ffi::c_uint::MAX as usize);
+
+			while !buf.is_empty() {
+				match self.read(buf, buf.len() as _) {
+					Ok(0) => break,
+					Ok(n) => {
+						buf = &mut buf[n as usize..];
+					},
+					Err(e) => return Err(io::Error::other(Box::new(e))),
+				}
+			}
+			if !buf.is_empty() {
+				Err(io::Error::new(io::ErrorKind::UnexpectedEof, "failed to fill whole buffer"))
+			} else {
+				Ok(())
+			}
+		}
+	}
+
+
+	impl io::Write for File {
+		fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+			debug_assert!(buf.len() <= core::ffi::c_uint::MAX as usize);
+
+			File::write(self, buf).map_err(|e| io::Error::other(Box::new(e)))
+			                      .map(|v| v as usize)
+		}
+
+		fn flush(&mut self) -> io::Result<()> {
+			File::flush(self).map_err(|e| io::Error::other(Box::new(e)))?;
+			Ok(())
+		}
+	}
+
+
+	impl io::Seek for File {
+		fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
+			#[cfg(debug_assertions)]
+			match pos {
+				io::SeekFrom::Start(v) => assert!(v <= core::ffi::c_uint::MAX as u64),
+				io::SeekFrom::End(v) => assert!(v.abs() <= core::ffi::c_int::MAX as i64),
+				io::SeekFrom::Current(v) => assert!(v.abs() <= core::ffi::c_int::MAX as i64),
+			}
+
+			File::seek(self, pos.into()).and_then(|_| File::tell(self))
+			                            .map(|v| v as _)
+			                            .map_err(|e| io::Error::other(Box::new(e)))
+		}
+
+
+		#[cfg(feature = "portable-io")]
+		fn stream_len(&mut self) -> io::Result<u64> {
+			let old_pos = self.stream_position()?;
+
+			self.seek(crate::seek::SeekFrom::End(0))
+			    .map_err(|e| io::Error::other(Box::new(e)))?;
+			let len = self.stream_position()?;
+
+			if old_pos != len {
+				self.seek(crate::seek::SeekFrom::Start(old_pos as _))
+				    .map_err(|e| io::Error::other(Box::new(e)))?;
+			}
+
+			Ok(len)
+		}
+
+		#[cfg(feature = "portable-io")]
+		fn stream_position(&mut self) -> io::Result<u64> {
+			File::tell(self).map(|v| v as _)
+			                .map_err(|e| io::Error::other(Box::new(e)))
+		}
+
+		#[cfg(feature = "portable-io")]
+		fn rewind(&mut self) -> portable_io::Result<()> {
+			self.seek(crate::seek::SeekFrom::Start(0))
+			    .map_err(|e| io::Error::other(Box::new(e)))?;
+			Ok(())
+		}
+	}
+
+
+	impl Into<io::SeekFrom> for crate::seek::SeekFrom {
+		fn into(self) -> io::SeekFrom {
+			match self {
+				crate::seek::SeekFrom::Start(v) => io::SeekFrom::Start(v as _),
+				crate::seek::SeekFrom::Current(v) => io::SeekFrom::Current(v as _),
+				crate::seek::SeekFrom::End(v) => io::SeekFrom::End(v as _),
+			}
+		}
+	}
+
+	impl From<io::SeekFrom> for crate::seek::SeekFrom {
+		fn from(other: io::SeekFrom) -> Self {
+			match other {
+				io::SeekFrom::Start(v) => Self::Start(v as _),
+				io::SeekFrom::End(v) => Self::End(v as _),
+				io::SeekFrom::Current(v) => Self::Current(v as _),
+			}
 		}
 	}
 }
